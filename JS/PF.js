b3a61e7f197752e1f5c23b98977e3e8ffeba9856
Neil-Lin-0404 <c84327515@gmail.com>
2025-08-22 00:03:34 +0800

    I fucking fix this git commit

--- JS/PF.js
+++ JS/PF.js
@@ -1,4 +1,35 @@
-function SubmitTask()
+var taskList = document.getElementById("lowerTask");
+document.getElementById("deleteBtn").addEventListener("click", deleteTask);
+function SubmitTask() {   
+    var taskNameInput = document.getElementById("AddTask").value;
+    if(taskNameInput === "") {
+        alert("Please enter a task name.");
+        return;
+    }
+    var Singletask = document.createElement("div");
+    Singletask.className = "Tasks";
+
+    var completeButton = document.createElement("button");
+    completeButton.className = "completeBtn";
+    completeButton.textContent = "";
+
+
+    var deleteButton = document.createElement("button");
+    deleteButton.className = "deleteBtn";
+    deleteButton.textContent = "Delete";
+
+    var taskName = document.createElement("p");
+    taskName.className = "TaskName";
+    taskName.textContent = taskNameInput;
+
+    Singletask.append(completeButton);
+        Singletask.append(taskName);
+    Singletask.append(deleteButton);
+
+    taskList.append(Singletask);
+    document.getElementById("AddTask").value = "";
+}
+function deleteTask()
 {
-    
+                   
 }
